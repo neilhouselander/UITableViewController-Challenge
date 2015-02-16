fd0dc3f00d106c71838e46894eed380173c5fb55
Neil Houselander <neilhouselander@sky.com>
2015-02-16 19:48:21 +0000

    Initial Commit

---
+++ UITableVIewController Challenge Solution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableVIewController Challenge Solution
+//
+//  Created by Neil Houselander on 16/02/2015.
+//  Copyright (c) 2015 Neil Houselander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
